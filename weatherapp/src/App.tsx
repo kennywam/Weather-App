3a38a81d9b7676326542e07ad7b0e7d280833293
Kennedy Wambua <kennywambu254@gmail.com>
2023-08-17 16:21:36 +0300

    Search functionality

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -1,9 +1,68 @@
-const App = () => {
+import { ChangeEvent, useState } from "react";
+import { optionType } from "./types";
+
+
+const App = (): JSX.Element => {
+  //states
+  const [term, setTerm] = useState<string>("");
+  const [options, setOptions] = useState<[]>([]);
+  const key = "f495d7f97a6e57761e65fbd47325f9b1";
+
+  const getSearchOptions = (value: string) => {
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${key}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const extractedOptions = data.map((item: { name: string }) => ({
+        name: item.name,
+      }));
+      setOptions(extractedOptions);
+    });
+};
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim();
+    setTerm(value);
+
+    if (value === "") return;
+    getSearchOptions(value);
+  };
+
+  const onOptionSelect = (option: optionType) => {
+     fetch(
+       `https://api.openweathermap.org/data/3.0/onecall?lat=${option.lat}&lon=${option.lon}&units=metric&appid=${key}`);
+  }
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400  h-[100vh] w-full">
-      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
-        <h1 className="text-4xl">Weather Forecast</h1>
-        <p>Search for a place to know the Weather</p>
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+        {" "}
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+        <p className="text-sm mt-2">Search for a place to check the Weather</p>
+        <div className="relative flex mt-10 md:mt-4">
+          {" "}
+          <input
+            type="text"
+            value={term}
+            className="px-2 py-1 rounded-l-md border-2 border-white"
+            onChange={onInputChange}
+          />
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number):JSX.Element => (
+              <li key={option.name + "-" + index}>
+                <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer" onClick={() => onOptionSelect(option)}>
+                  {option.name}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer">
+            search
+          </button>
+        </div>
       </section>
     </main>
   );
