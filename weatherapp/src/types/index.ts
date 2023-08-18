c8842c1f8c2464db8d7aec7a384d2dd4fc9c3d50
Kennedy Wambua <kennywambu254@gmail.com>
2023-08-18 03:47:53 +0300

    indicating types

--- weatherapp/src/types/index.ts
+++ weatherapp/src/types/index.ts
@@ -1,5 +1,19 @@
 export type optionType = {
-    name: string
-    lat: number
-    lon:number
-}
+  name: string;
+  lat: number;
+  lon: number;
+};
+
+export type weatherType = {
+  name: string;
+  country: string;
+    weather: [{ main: string; description: string;  icon: string}];
+    main: {
+    temp: number;
+    humidity: number;
+    };
+    wind: {
+        speed: number;
+    }
+    
+};
