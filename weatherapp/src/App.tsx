ad06a8864c58fa1f0a8215c7b6abc28263e07662
Kennedy Wambua <kennywambu254@gmail.com>
2023-08-18 03:43:04 +0300

    Render components

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -1,95 +1,24 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { optionType } from "./types";
+import Search from "./components/Search";
+import useWeather from "./hooks/useWeather";
+import Weather from "./components/Weather";
 
 const App = (): JSX.Element => {
-  //states
-  const [term, setTerm] = useState<string>("");
-  const [options, setOptions] = useState<[]>([]);
-  const [city, setCity] = useState<optionType | null>(null);
-
-  const key = "f495d7f97a6e57761e65fbd47325f9b1";
-
-  const getSearchOptions = async (value: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${key}`
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data))
-      .catch((e) => console.log({ e }));
-  };
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim();
-    setTerm(value);
-
-    if (value === "") return;
-    getSearchOptions(value);
-  };
-
-  const getForecast = (city: optionType) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${key}`
-    )
-      .then((res) => res.json())
-      .then((data) => console.log({ data }))
-      .catch((error) => {
-        console.error("Error fetching weather data:", error);
-      });
-  };
-  const onSubmit = () => {
-    if (!city) return;
-
-    getForecast(city);
-  };
-  const onOptionSelect = (option: optionType) => {
-    setCity(option);
-  };
-
-  useEffect(() => {
-    if (city) {
-      setTerm(city.name);
-      setOptions([]);
-    }
-  }, [city]);
+  const { weather, options, term, onOptionSelect, onSubmit, onInputChange } =
+     useWeather();
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400  h-[100vh] w-full">
-      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
-        {" "}
-        <h1 className="text-4xl font-thin">
-          Weather <span className="font-black">Forecast</span>
-        </h1>
-        <p className="text-sm mt-2">Search for a place to check the Weather</p>
-        <div className="relative flex mt-10 md:mt-4">
-          {" "}
-          <input
-            type="text"
-            value={term}
-            className="px-2 py-1 rounded-l-md border-2 border-white"
-            onChange={onInputChange}
-          />
-          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
-            {options.map(
-              (option: optionType, index: number): JSX.Element => (
-                <li key={option.name + "-" + index}>
-                  <button
-                    className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer"
-                    onClick={() => onOptionSelect(option)}
-                  >
-                    {option.name}
-                  </button>
-                </li>
-              )
-            )}
-          </ul>
-          <button
-            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer"
-            onClick={onSubmit}
-          >
-            search
-          </button>
-        </div>
-      </section>
+      {weather ? (
+        <Weather data={weather} />
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   );
 };
