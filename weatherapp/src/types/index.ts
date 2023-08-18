94a30dc74e06a0080759250ba049064cb9eebd55
Kennedy Wambua <kennywambu254@gmail.com>
2023-08-18 04:06:34 +0300

    Adding types

--- weatherapp/src/types/index.ts
+++ weatherapp/src/types/index.ts
@@ -7,13 +7,18 @@ export type optionType = {
 export type weatherType = {
   name: string;
   country: string;
-    weather: [{ main: string; description: string;  icon: string}];
-    main: {
+  weather: [{ main: string; description: string; icon: string }];
+  main: {
     temp: number;
     humidity: number;
-    };
-    wind: {
-        speed: number;
-    }
-    
+  };
+  wind: {
+    speed: number;
+    gust: number;
+    deg: number;
+  };
 };
+
+
+
+
