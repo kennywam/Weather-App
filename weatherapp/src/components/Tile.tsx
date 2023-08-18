bcda27b27069ffd65c0dc3b2c5de3490447c9e89
Kennedy Wambua <kennywambu254@gmail.com>
2023-08-18 04:05:31 +0300

    Display fix

--- weatherapp/src/components/Tile.tsx
+++ weatherapp/src/components/Tile.tsx
@@ -4,7 +4,8 @@ import Wind from "./Icons/Wind";
 type Props = {
   icon: "wind" | "humidity";
   title: string
-  info: string | JSX.Element
+    info: string | JSX.Element
+  description?: string | JSX.Element
   
 };
 
@@ -13,17 +14,17 @@ const icons = {
   humidity: Humidity,
 };
 
-const Tile = ({
-  icon, title, info
-}: Props): JSX.Element => {
+const Tile = ({ icon, title, info, description }: Props): JSX.Element => {
   const Icon = icons[icon];
   return (
-    <div>
-      <article className="w-[140px] h-[130px] text-zinc-700 bg-white/20 backdrop-blur-ls rounded drop-shadow-lg p-2 mb-5 flex flex-col justify-between">
-        {" "}
-        <Icon />
-      </article>
-    </div>
+     <article className="w-[140px] h-[130px] text-zinc-700 bg-white/20 backdrop-blur-ls rounded drop-shadow-lg p-2 mb-5 flex flex-col justify-between">
+      <div className="flex items-center text-sm font-bold">
+        <Icon /> <h4 className="ml-1">{title}</h4>
+      </div>
+      <h3 className="mt-2 text-lg">{info}</h3>
+
+      <div className="text-xs font-bold">{description}</div>
+    </article>
   );
 };
 export default Tile;
